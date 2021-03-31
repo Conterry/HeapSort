ec8b86f9362500e8be9e78a4acadca55892bb886
unknown <Conterry>
2021-03-31 13:58:07 +0300

    sort added

--- HeapSort.js
+++ HeapSort.js
@@ -1,43 +1,45 @@
-let array = [123, 234,345, 567, 678]
+let array = [123, 234,345, 567, 678];
+let arrayLength = array.length;
+let newArray = [];
+function setDoughter(iter){
+	var iLeft = iter*2+1;
+	var iRight = iter*2+2;
+	var res = 0;
 
-
-var heapLength = 100000;
-
-function setDoughter(i){
-	let iLeft = array[i*2+1];
-	let iRight = array[i*2+2];
+	if(iLeft < arrayLength-1){
+		setDoughter(iLeft);
+	}
+	if(array[iLeft] > array[iRight] && array[iLeft]>array[iter]){
+		res = array[iter];
+		array[iter] = array[iLeft];
+		array[iLeft] = res
+	}
 	
-	if(i*2+1 < array.length){
-		setDoughter(i*2+1);
-		if(iLeft > iRight && iLeft>array[i]){
-			let res = array[i];
-			array[i] = iLeft;
-			iLeft = res
-		}
+	if(iRight < arrayLength-1){
+		setDoughter(iRight);
 	}
-	if(i*2+2 < array.length){
-		setDoughter(i*2+2);
-		if(iLeft < iRight && iRight>array[i]){
-			let res = array[i];
-			array[i] = iRight;
-			iRight = res;
-		}
-		
+	if(array[iLeft] < array[iRight] && array[iRight]>array[iter]){
+		res = array[iter];
+		array[iter] = array[iRight];
+		array[iRight] = res;
 	}
-	if(iLeft<iRight){
-		let res = iLeft;
-		iLeft = iRight;
-		iRight = res;
+
+	if(array[iLeft]<array[iRight]){
+		res = array[iLeft];
+		array[iLeft] = array[iRight];
+		array[iRight] = res;
 	}
 }
 
-function refactor(i){
-	setDoughter(i+1)
+function start(){
+	var i=0;
+	while(array[0]!=null){
+		setDoughter(0);
+		newArray.push(array[0]);
+		array.splice(0, 1);
+	}
 }
 
-setDoughter(0);
-for( var i = 0; i<heapLength-1; i++){
-	setDoughter(i);
-}
+start();
 
-console.log(array.join(" "));
+console.log(newArray.join(" "));
